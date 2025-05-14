0ffd681381d6dbb552cf417d380981589e8647d4
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-05-14 14:39:14 +0300

    Add files via upload

---
+++ Программы/Примеры/1/1.cpp
@@ -0,0 +1,24 @@
+﻿#include "stdio.h"
+#include "conio.h"
+
+int main(void)
+{
+	double x = 1.0, q = 0.5, sum = 0.0;
+	int n = 3;
+	__asm {
+		mov ecx, n
+	l1:
+		fld q
+		fld x
+		fmul st(0), st(1)
+		fstp x
+		fld x
+		fld sum
+		fadd st(0), st(1)
+		fstp sum
+		loop l1
+	}
+	printf("sum = %f\n", sum);
+	_getch();
+	return 0;
+}
---
+++ Программы/Примеры/2/2.cpp
@@ -0,0 +1,34 @@
+﻿#include "stdio.h"
+#include "conio.h"
+
+int main(void)
+{
+	double n = 1.0, s = 120.0, p = 0.05, q = 100.0;
+	__asm {
+		fld s
+	l1:
+		ffree st(1)
+		ffree st(2)
+		ffree st(3)
+		ffree st(4)
+		fld1
+		fld n
+		fadd st(0), st(1)
+		fstp n
+		fld q
+		fld st(0)
+		fld p
+		fmul st(0), st(1)
+		fadd st(0), st(1)
+		fst q
+		fcom s
+		fstsw ax
+		sahf
+		jb l1
+		fstp q
+	}
+	printf("God => %g\n", n);
+	printf("Kol-vo gektarov ==> %g\n", q);
+	_getch();
+	return 0;
+}
