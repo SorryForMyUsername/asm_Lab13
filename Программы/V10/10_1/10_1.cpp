a84e4f4afab7af4e6e943bd397060423c0fff3f8
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-05-27 02:44:21 +0300

    Add files via upload

---
+++ Программы/V10/10_1/10_1.cpp
@@ -0,0 +1,14 @@
+﻿#include "stdio.h"
+#include "conio.h"
+
+int main(void)
+{
+	double n = 1.0, s = 120.0, p = 0.05, q = 100.0;
+	__asm {
+		
+	}
+	printf("God => %g\n", n);
+	printf("Kol-vo gektarov ==> %g\n", q);
+	_getch();
+	return 0;
+}
---
+++ Программы/V10/10_2/10_2.cpp
@@ -0,0 +1,27 @@
+﻿#include "stdio.h"
+#include "conio.h"
+
+int main(void)
+{
+	double a = 1.3, x;
+	__asm {
+		fld1
+		fldz
+		fld a
+		fld1
+	l1:
+		fld st(1)
+		fdivr st(0), st(1)
+		fadd st(0), st(2)
+		fxch st(1)
+		fxch st(3)
+		fcomi st(0), st(1)
+		fxch st(1)
+		ja l2
+
+		fstp x
+	}
+	printf("x = %g\n", x);
+	_getch();
+	return 0;
+}
